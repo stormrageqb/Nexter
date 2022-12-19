8e59a9f2e00741449507d0162e6c1bb304b0e61b
stormrageqb <stormrage.qb@gmail.com>
2022-12-19 16:04:01 -0600

    Install React Vite template along with component test and initial styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Test from "./components/Test";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Nexter</h1>
+      <Test />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Test.jsx
@@ -0,0 +1,9 @@
+const Test = () => {
+  return (
+    <>
+      <p className="test">This is a test component</p>
+    </>
+  );
+};
+
+export default Test;
