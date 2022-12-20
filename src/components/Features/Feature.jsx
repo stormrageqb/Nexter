fec30be59e0bab414f2e317d13a06389ceb7b1f2
stormrageqb <stormrage.qb@gmail.com>
2022-12-20 10:18:22 -0600

    Map over feature data and render each feature in grid

--- src/components/Features/Feature.jsx
+++ src/components/Features/Feature.jsx
@@ -2,16 +2,16 @@ import "./Feature.scss";
 import { features } from "./FeatureData";
 
 const Feature = () => {
-  return (
-    <>
-      <div className="feature">feature</div>
-      <div className="feature">feature</div>
-      <div className="feature">feature</div>
-      <div className="feature">feature</div>
-      <div className="feature">feature</div>
-      <div className="feature">feature</div>
-    </>
-  );
+  const featureList = features.map((feature) => (
+    <div key={feature.id} className="feature">
+      <svg className="feature__icon">
+        <use href={feature.svg}></use>
+      </svg>
+      <h4 className="heading-4">{feature.heading}</h4>
+      <p className="feature__text">{feature.text}</p>
+    </div>
+  ));
+  return <>{featureList}</>;
 };
 
 export default Feature;
