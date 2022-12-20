7816eca6196a7e5da9e77498db324be948f99337
stormrageqb <stormrage.qb@gmail.com>
2022-12-20 10:53:04 -0600

    Finish features section and add responsive grid to parent grid

--- src/components/Features/Feature.jsx
+++ src/components/Features/Feature.jsx
@@ -7,7 +7,7 @@ const Feature = () => {
       <svg className="feature__icon">
         <use href={feature.svg}></use>
       </svg>
-      <h4 className="heading-4">{feature.heading}</h4>
+      <h4 className="heading-4 heading-4--dark">{feature.heading}</h4>
       <p className="feature__text">{feature.text}</p>
     </div>
   ));
