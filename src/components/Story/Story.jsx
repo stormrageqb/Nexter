f81f92b35348140d16ef1fdb57b4f1f33dac08dc
stormrageqb <stormrage.qb@gmail.com>
2022-12-21 15:12:40 -0600

    Add content to Story component along with initial styles

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -4,7 +4,17 @@ const Story = () => {
   return (
     <>
       <div className="story__pictures">Pictures</div>
-      <div className="story__content">Content</div>
+      <div className="story__content">
+        <h3 className="heading-3 mb-sm">Happy Customers</h3>
+        <h2 className="heading-2 heading-2--dark mb-md">
+          &ldquo;The best decision of our lives&rdquo;
+        </h2>
+        <p className="story__text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus fuga
+          tempore doloribus!{" "}
+        </p>
+        <button className="btn">Find your own home</button>
+      </div>
     </>
   );
 };
