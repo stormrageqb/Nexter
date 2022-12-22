39ee403e552a522468a04514d9997e4b5882b1c3
stormrageqb <stormrage.qb@gmail.com>
2022-12-22 15:54:42 -0600

    Add like and unlike classes to each mapped svg like button via useState hook

--- src/components/Homes/Homes.jsx
+++ src/components/Homes/Homes.jsx
@@ -1,15 +1,49 @@
 import "./Homes.scss";
 import { homes } from "./HomeData";
+import { useState } from "react";
 
 const Homes = () => {
+  const [toggleLike, setToggleLike] = useState({
+    toggle: [],
+    index: "",
+  });
+  const handleToggleLike = (index) => {
+    setToggleLike((state) => ({
+      ...state,
+      toggle: state.toggle.filter((t) => t !== index),
+    }));
+  };
+
+  const handleToggleDislike = (index) => {
+    setToggleLike((state) => ({
+      ...state,
+      toggle: [...state.toggle, index],
+    }));
+  };
   return (
     <section className="homes">
-      {homes.map((home) => (
+      {homes.map((home, i) => (
         <div key={home.id} className="home">
           <img src={home.img} alt={home.alt} className="home__img" />
-          <svg className="home__like">
-            <use href="src/assets/img/sprite.svg#icon-heart-full"></use>
-          </svg>
+
+          {toggleLike.toggle.includes(i) ? (
+            <svg
+              key={i}
+              onClick={() => handleToggleLike(i)}
+              className="home__like home__like--active"
+            >
+              <use href="src/assets/img/sprite.svg#icon-heart-full"></use>
+            </svg>
+          ) : (
+            <svg
+              key={i}
+              onClick={() => handleToggleDislike(i)}
+              className="home__like"
+            >
+              <use href="src/assets/img/sprite.svg#icon-heart-full"></use>
+            </svg>
+          )}
+
           <h5 className="home__name">{home.heading}</h5>
           <div className="home__location">
             <svg>
@@ -41,6 +75,7 @@ const Homes = () => {
             </svg>
             <p>{home.price}</p>
           </div>
+
           <button className="btn home__btn">Contact realtor</button>
         </div>
       ))}
