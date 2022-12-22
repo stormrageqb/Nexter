42bb8434ae2d7aeed7afa961a14426bdc29cd08a
stormrageqb <stormrage.qb@gmail.com>
2022-12-22 11:42:03 -0600

    Add data for homes and create grid for cards in Homes.scss

--- src/components/Homes/Homes.jsx
+++ src/components/Homes/Homes.jsx
@@ -1,7 +1,17 @@
 import "./Homes.scss";
+import { homes } from "./HomeData";
 
 const Homes = () => {
-  return <section className="homes">Homes</section>;
+  return (
+    <section className="homes">
+      {homes.map((home) => (
+        <div key={home.id} className="home">
+          Home
+        </div>
+      ))}
+      <div className="home"></div>
+    </section>
+  );
 };
 
 export default Homes;
