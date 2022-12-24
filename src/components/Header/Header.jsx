d6fcca0b77ac26957a776c2580eef88e90dcbd69
stormrageqb <stormrage.qb@gmail.com>
2022-12-24 10:17:11 -0600

    Add content to header along with adjustments to logos and text size and background

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import "./Header.scss";
 
 const Header = () => {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header">
+      <img src="src\assets\img\logo.png" alt="logo" className="header__logo" />
+      <h3 className="heading-3">Your own home:</h3>
+      <h1 className="heading-1">The ultimate personal freedom</h1>
+      <button className="btn">View our properties</button>
+      <div className="header__seenon-text">Seen on</div>
+      <div className="header__seenon-logos">
+        <img src="src\assets\img\logo-bbc.png" alt="BBC logo" />
+        <img src="src\assets\img\logo-forbes.png" alt="BBC logo" />
+        <img src="src\assets\img\logo-techcrunch.png" alt="BBC logo" />
+        <img src="src\assets\img\logo-bi.png" alt="BBC logo" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
