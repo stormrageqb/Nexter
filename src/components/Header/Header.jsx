f3e271fb94894e02e3b53ebb0f1c9393ef1a0ad9
stormrageqb <stormrage.qb@gmail.com>
2022-12-24 10:28:22 -0600

    Align content in header grid

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <img src="src\assets\img\logo.png" alt="logo" className="header__logo" />
       <h3 className="heading-3">Your own home:</h3>
       <h1 className="heading-1">The ultimate personal freedom</h1>
-      <button className="btn">View our properties</button>
+      <button className="btn header__btn">View our properties</button>
       <div className="header__seenon-text">Seen on</div>
       <div className="header__seenon-logos">
         <img src="src\assets\img\logo-bbc.png" alt="BBC logo" />
