975634c051978c88dd3225d8b517f658e4bdbd06
stormrageqb <stormrage.qb@gmail.com>
2022-12-24 12:41:09 -0600

    Add Realtor data and render it to Realtor section of Header component

--- src/components/Header/Realtors/Realtors.jsx
+++ src/components/Header/Realtors/Realtors.jsx
@@ -1,7 +1,31 @@
 import "./Realtors.scss";
+import { realtorsData } from "./RealtorsData";
 
 const Realtors = () => {
-  return <div className="realtors">Top 3 realtors</div>;
+  return (
+    <div className="realtors">
+      <h3 className="heading-3">Top 3 Realtors</h3>
+      <div className="realtors__list">
+        {realtorsData.map((realtor) => {
+          return (
+            <>
+              <img
+                src={realtor.img}
+                alt={realtor.name}
+                className="realtors__img"
+              />
+              <div className="realtors__details">
+                <h4 className="heading-4 heading-4--light">{realtor.name}</h4>
+                <p className="realtors__sold">
+                  {realtor.housesSold} houses sold
+                </p>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Realtors;
---
+++ src/components/Header/Realtors/RealtorsData.js
@@ -0,0 +1,17 @@
+export const realtorsData = [
+  {
+    img: "src/assets/img/realtor-1.jpeg",
+    name: "Erik Feinman",
+    housesSold: 245,
+  },
+  {
+    img: "src/assets/img/realtor-2.jpeg",
+    name: "Kim Brown",
+    housesSold: 212,
+  },
+  {
+    img: "src/assets/img/realtor-3.jpeg",
+    name: "Toby Ramsey",
+    housesSold: 198,
+  },
+];
