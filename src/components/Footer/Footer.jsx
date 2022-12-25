b6c4b6a4e9b9c1db0060b15553ef73d36e17d798
stormrageqb <stormrage.qb@gmail.com>
2022-12-25 10:32:17 -0600

    Add content to footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,42 @@
 import "./Footer.scss";
 
 const Footer = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <ul className="nav">
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Find your dream home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Request proposal
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Download home planner
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contact us
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Submit your property
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Come work with us
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
